9134a092eed4abd4c7205149698f8e4a7752ceb1
xytungg <79676866+xytungg@users.noreply.github.com>
2022-05-30 16:53:45 +0800

    Delete week15-1_PlaySound.cpp

--- week15-1_PlaySound.cpp
+++
@@ -1,7 +0,0 @@
-///#include <mmsystem.h>//上週教
-#include <windows.h>///另一種寫法，結果一樣
-int main()
-{           ///                  上週教SND_ASYNC
-    PlaySound("07042111.wav", NULL, SND_SYNC);
-}///Compile成功，但是link失敗
-/// .c .cpp =compile=> .o =link=> .exe
